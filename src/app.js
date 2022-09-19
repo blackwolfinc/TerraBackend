7c4cec714c32421cfb23a052817c22426b48d866
najibean <najibullah89@gmail.com>
2022-09-19 22:23:02 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(compression());
+
+app.get('/test', (req, res) => {
+  return res.status(200).send('PROJECT BAGAS!');
+});
+
+app.listen(3600, () => console.log('App is running at port 3600'));
