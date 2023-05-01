03309b91b490be703e26b76e91b3ffef9fe7f8ce
najibean <najibullah89@gmail.com>
2023-05-01 23:47:44 +0800

    add test route inside index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,8 +2,13 @@ const { Router } = require('express');
 const productRouters = require('./product.router');
 const blogRouters = require('./blog.router');
 const apiKeyAuthentication = require('../middlewares/apiKeyAuthentication');
+const responseHandler = require('../helpers/responseHandler');
 
 const router = Router();
+
+router.get('/test', (req, res, next) => {
+  responseHandler.succes(res, 'Test route inside /index.js');
+});
 
 router.use('/product', apiKeyAuthentication, productRouters);
 router.use('/blog', apiKeyAuthentication, blogRouters);
