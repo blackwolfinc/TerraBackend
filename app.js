b0ac629165c917764035ee65a0f4e4ecf931b06c
najibean <najibullah89@gmail.com>
2023-05-01 11:39:21 +0800

    update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(morgan('dev'));
 app.use(compression());
 
 app.get('/test', (req, res) => {
-  return res.status(200).send('PROJECT BAGAS ya najib!');
+  return res.status(200).send('PROJECT BAGAS tested in pulling from git to cPannel');
 });
 
 app.listen(PORT, () => console.log('App is running at port ' + PORT));
